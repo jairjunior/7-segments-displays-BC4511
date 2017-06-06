e75ce4f3b6f1b7dfb72beb89f538f798055c5213
Jair Junior <contato@jairjunior.eng.br>
2017-06-06 01:40:08 -0300

    More comments were added to the code.

--- sevenSegmentsDisplay-BC4511.ino
+++ sevenSegmentsDisplay-BC4511.ino
@@ -1,33 +1,37 @@
-/* Numero de displays */
+/* Numero de displays ou de digitos */
 #define N 4
 
-/* Displays na ordem da esquerda para a direita*/
+/* Displays conectados na ordem da esquerda para a direita*/
 int sevenSegmentsDisplays[4] = {2,3,4,5};
-
-/* Entradas do CI  => A,B,C,D */
+/* Entradas do CI   => A,B,C,D */
 int bc4511Inputs[4] = {6,7,8,9};
-
 /* Códigos BCD para representar números de 0 a 10*/
 byte bcdCode[10] = {B0000, B0001, B0010, B0011, B0100, B0101, B0110, B0111, B1000, B1001};
-
-/* Máscara de bits para as entradas do CI */
+/* Máscara de bits para as entradas do CI (A,B,C e D) */
 byte mask[N] = {B0001, B0010, B0100, B1000};
-
-/* Variavel que armazena dígitos separados de unidade, dezena, centena... */
+/* Variavel que armazena os dígitos de unidade, dezena, centena... */
 unsigned int digitos[N];
-
 /* Variáveis para controlar incremento do contador através do tempo */
 unsigned long tempo;
 int contador = 0;
 
+
+/*-----------------------------------------------------
+ * Função setup()
+ ------------------------------------------------------*/
 void setup() {
+  /* Configura pinos de saída */
   for(int i = 0; i < N; i++){
     pinMode(sevenSegmentsDisplays[i], OUTPUT);
     pinMode(bc4511Inputs[i], OUTPUT);
   }
+  /* Inicia variável contador */
   tempo = millis();
 }
 
+/*-----------------------------------------------------
+ * Função loop()
+ ------------------------------------------------------*/
 void loop() {
   digitos[3] = contador%10;
   digitos[2] = (contador/10)%10;
@@ -37,22 +41,19 @@ void loop() {
     /* Laço para varrer os displays sequencialmente */
     /* Varre da esquerda para direita, começando da dezenaMilhar */
     for(int i = 0; i < N; i++){
-      
         /* Laço para setar valores A, B, C, D no BC4511 (código BCD)*/
         for(int j = 0; j < N; j++){
-
-          if( bcdCode[digitos[i]] & mask[j] )
-            digitalWrite(bc4511Inputs[j], HIGH);
-          else
-            digitalWrite(bc4511Inputs[j], LOW);
+            if( bcdCode[digitos[i]] & mask[j] )
+              digitalWrite(bc4511Inputs[j], HIGH);
+            else
+              digitalWrite(bc4511Inputs[j], LOW);
         }
-
         /* Liga e desliga display rapidamente */
         digitalWrite(sevenSegmentsDisplays[i], HIGH);
         delay(5);
         digitalWrite(sevenSegmentsDisplays[i], LOW);
     }
-
+    /* Confere o tempo decorrido e faz o incremento do contador se necessário */
     if((millis() - tempo) > 1000){
       tempo = millis();
       contador++;
